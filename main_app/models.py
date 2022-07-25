64c1bce711a7877eef305e3502cc142c72f50fad
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-24 21:34:00 -0400

    lab 1 exercise 1 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    finch_name = models.CharField(max_length=15)
+    pub_date = models.DateTimeField('date published')
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Add the Finch class & list and view function below the imports
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name):
+    self.name = name
+
+finches = [
+  Finch('Lolo'),
+  Finch('Sachi'),
+  Finch('Raven')
+]
