117cffacae1817c56df682daa049097ee802dd31
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-24 22:08:22 -0400

    added __str__ override method in Finch model

--- main_app/models.py
+++ main_app/models.py
@@ -5,3 +5,6 @@ class Finch(models.Model):
     name = models.CharField(max_length=15)
     description = models.TextField(max_length=100)
     wingspan = models.IntegerField()
+
+    def __str__(self):
+        return self.name
