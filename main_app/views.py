d0a71466071a6ab26f04292d51f4aa94c842529a
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-25 00:43:28 -0400

    lab 5 exercise 2 completed.... FinchCollector Lab is completed

--- main_app/views.py
+++ main_app/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Add the following import
 from django.http import HttpResponse
-from .models import Finch
+from .models import Finch, Toy
 from .forms import FeedingForm
 
 # views.py
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class FinchCreate(CreateView):
     model = Finch
-    fields = '__all__'
+    fields = ['name', 'description', 'wingspan']
     success_url = '/finches/'
 class FinchUpdate(UpdateView):
     model = Finch
@@ -45,8 +45,13 @@ def finches(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=finch.toys.all().values_list('id'))
   feeding_form = FeedingForm()
-  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
+  return render(request, 'finches/detail.html', { 
+    'finch': finch, 
+    'feeding_form': feeding_form, 
+    'toys': toys_finch_doesnt_have 
+  })
 
 def add_feeding(request, finch_id):
     # create a ModelForm instance using the data in request.POST
@@ -58,4 +63,30 @@ def add_feeding(request, finch_id):
     new_feeding = form.save(commit=False)
     new_feeding.finch_id = finch_id
     new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
   return redirect('detail', finch_id=finch_id)
